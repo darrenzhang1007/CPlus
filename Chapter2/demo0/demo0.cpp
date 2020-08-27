052b0dafd2d3ad3aca7d1beb4601238eb5b74bd7
darrenmondzhang <785320051@qq.com>
2020-08-27 17:25:37 +0800

    本代码作用：使用断点调试工具，对变量地址进行监视，并且查看不同的数据类型占用字节多少

--- Chapter2/demo0/demo0.cpp
+++ Chapter2/demo0/demo0.cpp
@@ -1,11 +1,11 @@
-// demo0.cpp : 定义控制台应用程序的入口点。
-//
+// 本代码作用：使用断点调试工具，对变量地址进行监视，
+// 并且查看不同的数据类型占用字节多少
 
 #include "stdafx.h"
 
 void  main(void)
 {
-	char a[10] = "a";
+	char a[10] = "a"; // 10 bytes
 	short int s = 97;
 	int m = 97;
 	long int n = 97;
@@ -13,7 +13,8 @@ void  main(void)
 	double d = 97.0;
 	long double k = 97.0;
 	bool b = true;
-	wchar_t w[10] = L"a";
-
+	wchar_t w[10] = L"a";  // 20 bytes
+	// wchar_t:每个字符占用2bytes
+	// char：每个字符占用1bytes
 }
 
