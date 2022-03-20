ca945dc4a2f8c7b2aca2c665701c0ed6ce227304
DarrenmondZhang <darrenzhang1007@163.com>
2022-03-20 23:45:01 +0800

    chapter04 ing

--- C++_Primer_Plus/chapter04/4-17_use_new.cpp
+++ C++_Primer_Plus/chapter04/4-17_use_new.cpp
@@ -1,33 +1,45 @@
+/*** 
+ * @Author       : DarrenZhang
+ * @Date         : 2022-03-20 17:11:00
+ * @LastEditTime : 2022-03-20 17:13:32
+ * @LastEditors  : DarrenZhang
+ * @Description  : 
+ * @FilePath     : /CPlus/C++_Primer_Plus/chapter04/4-17_use_new.cpp
+ */
 /*************************************************************************
-	> File Name: homework.cpp
-	> Author: DarrenZhang
-	> Mail: darrenzhang1007@163.com
-	> Created Time: 2021-03-26-4:12 PM
+    > File Name: homework.cpp
+    > Author: DarrenZhang
+    > Mail: darrenzhang1007@163.com
+    > Created Time: 2021-03-26-4:12 PM
  ************************************************************************/
 #include <iostream>
 
 using namespace std;
 
-int main() {
-    int nights = 1001;
-    cout << "nights value = ";
-    cout << nights << ": location " << &nights << endl;
+int main()
+{
+    // int nights = 1001;
+    // cout << "nights value = ";
+    // cout << nights << ": location " << &nights << endl;
 
-    int *pt = new int;
-    *pt = 1001;
-    cout << "int ";
-    cout << "value = " << *pt << ": location = " << pt << endl;
+    // int *pt = new int;
+    // *pt = 1001;
+    // cout << "int ";
+    // cout << "value = " << *pt << ": location = " << pt << endl;
 
-    double *pd = new double;
-    *pd = 10000001.0;
+    // double *pd = new double;
+    // *pd = 10000001.0;
 
-    cout << "double ";
-    cout << "value = " << *pd << ": location = " << pd << endl;
-    cout << "location of pointer pd: " << &pd << endl;  // 取出指针的地址
-    cout << "size of pt = " << sizeof(pt);
-    cout << ": size of *pt = " << sizeof(*pt) << endl;
-    cout << "size of pd = " << sizeof pd;
-    cout << ": size of *pd = " << sizeof(*pd) << endl;
+    // cout << "double ";
+    // cout << "value = " << *pd << ": location = " << pd << endl;
+    // cout << "location of pointer pd: " << &pd << endl;  // 取出指针的地址
+    // cout << "size of pt = " << sizeof(pt);
+    // cout << ": size of *pt = " << sizeof(*pt) << endl;
+    // cout << "size of pd = " << sizeof pd;
+    // cout << ": size of *pd = " << sizeof(*pd) << endl;
+    double *pt = new double;
+    *pt = 100.1;
+    cout << "int value = " << *pt << ": location = " << pt << endl;
 
     return 0;
 }
--- C++_Primer_Plus/chapter04/4-18_arraynew.cpp
+++ C++_Primer_Plus/chapter04/4-18_arraynew.cpp
@@ -1,15 +1,20 @@
-/*************************************************************************
-	> File Name: homework.cpp
-	> Author: DarrenZhang
-	> Mail: darrenzhang1007@163.com
-	> Created Time: 2021-03-26-4:12 PM
- ************************************************************************/
+/***
+ * @Author       : DarrenZhang
+ * @Date         : 2022-03-20 18:02:58
+ * @LastEditTime : 2022-03-20 18:02:58
+ * @LastEditors  : DarrenZhang
+ * @Description  : 通过指针，访问动态数组中的元素
+ * @FilePath     : /CPlus/C++_Primer_Plus/chapter04/4-18_arraynew.cpp
+ */
 #include <iostream>
 
 using namespace std;
 
-int main() {
-    double *p3 = new double[3];
+int main()
+{
+    double *p3 = new double[3]; // 初始化一个能够存放3个double类型元素的动态数组
+
+    // p3指针指向动态开辟出来的内存空间，并将其当作数组名，访问元素
     p3[0] = 0.2;
     p3[1] = 0.5;
     p3[2] = 0.8;
@@ -17,6 +22,7 @@ int main() {
     cout << "p3[1] is " << p3[1] << "." << endl;
 
     p3 = p3 + 1;
+    // 原先p3指针指向的是数组中的第一个元素，现在p3指针往后移动一个元素，指向的是原始动态数组中的第二个元素
     cout << "Now p3[0] is " << p3[0] << "." << endl;
     cout << "Now p3[1] is " << p3[1] << "." << endl;
 
@@ -28,4 +34,3 @@ int main() {
 
     return 0;
 }
-
